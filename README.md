5b51726232fe0b14063c547adb62442dfca52959
mitkar241 <raktimhalder241@gmail.com>
2022-04-13 16:28:30 +0000

    Dev Setup Stable version

--- README.md
+++ README.md
@@ -2,51 +2,27 @@
 ---
 A terraform repository
 
-### vscode extensions
+### VS Code Setup
 ---
-```bash
-code --list-extensions
-```
-```
-4ops.terraform
-amazonwebservices.aws-toolkit-vscode
-```
-```bash
-code --install-extension amazonwebservices.aws-toolkit-vscode
-```
 
-### terraform installation
+![vscode-extn-md](vscode.md)
+
+### AWS IAM Setup
 ---
-```bash
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt update && sudo apt install terraform -y
-```
 
-### commands used
+![aws-iam-md](iam.md)
+
+### Terraform Setup
 ---
-```bash
-terraform init
-```
-```bash
-ls -a
-```
-```
-.  .. dev.setup.public.tf  .terraform  .terraform.lock.hcl
-```
 
-```bash
-terraform fmt
-```
-```bash
-terraform plan
-terraform apply -auto-approve
-```
-```bash
-terraform state list
-terraform show
-```
-```bash
-terraform plan -destroy
-terraform apply -destroy -auto-approve
-```
+![terraform-setup-md](terraform.md)
+
+### Remote SSH using VS Code
+---
+
+![remote-ssh-md](remotessh.md)
+
+### Commands Used
+---
+
+![cmds-used-md](commands.md)
--- dev-setup-public/dev.setup.public.tf
+++ dev-setup-public/dev.setup.public.tf
@@ -1,34 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region                  = "ap-south-1"
-  shared_credentials_file = "/home/raktim/.aws/credentials"
-  profile                 = "tagrant"
-  default_tags {
-    tags = {
-      #Environment = "Test"
-      Name  = "tagrant"
-      Owner = "mitkar241"
-    }
-  }
-}
-
-/*
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
-}
-*/
-
 # Create a VPC
 resource "aws_vpc" "tagrant" {
   cidr_block           = "10.0.0.0/24"
@@ -74,4 +43,61 @@ resource "aws_route" "tagrant" {
 resource "aws_route_table_association" "tagrant" {
   subnet_id      = aws_subnet.tagrant.id
   route_table_id = aws_route_table.tagrant.id
+}
+
+resource "aws_security_group" "tagrant" {
+  name        = "tagrant_sg"
+  description = "tagrant security group for dev"
+  vpc_id      = aws_vpc.tagrant.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["116.206.222.130/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Description = "tagrant security group"
+  }
+}
+
+resource "aws_key_pair" "tagrant" {
+  key_name   = "tagrant"
+  public_key = file("~/.ssh/tagrant.pub")
+}
+
+resource "aws_instance" "tagrant" {
+  ami                    = data.aws_ami.tagrant.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.tagrant.id
+  vpc_security_group_ids = [aws_security_group.tagrant.id]
+  subnet_id              = aws_subnet.tagrant.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  # apply -replace
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}.ssh.config.tpl", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/tagrant"
+      }
+    )
+    interpreter = var.host_os == "ubuntu" ? ["bash", "-c"] : ["Powershell", "-Command"]
+  }
+
+  tags = {
+    Description = "tagrant compute"
+  }
 }
